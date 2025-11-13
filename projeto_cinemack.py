53bdb87072e3707dabcb7b2cdf9ff1538b3d65fa
Guigspinho <84827957+Guigspinho@users.noreply.github.com>
2025-11-13 13:25:13 +0000

    menu

--- projeto_cinemack.py
+++ projeto_cinemack.py
@@ -1,1 +1,2 @@
-print("hello world")
+def main()
+print("Menu Principal\n1. Comprar ingressos para Filme 1 - Sessão 1\n2. Comprar ingressos para Filme 1 - Sessão 2\n3. Comprar ingressos para Filme 2 - Sessão 1\n4. Comprar ingressos para Filme 2 - Sessão 2\n5. Comprar ingressos para Filme 3 - Sessão 1\n6. Comprar ingressos para Filme 3 - Sessão 2\n7. Avaliar um filme\n8. Encerrar o dia e exibir o relatório")
