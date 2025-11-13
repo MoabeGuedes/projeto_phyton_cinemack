115b92c06c7f8fff220ec2cd5476a228373217d8
Guigspinho <84827957+Guigspinho@users.noreply.github.com>
2025-11-13 13:32:49 +0000

    listas vazias

--- projeto_cinemack.py
+++ projeto_cinemack.py
@@ -1,2 +1,11 @@
+#Guilherme Gomes Pinho - Moabe da Silva Guedes Rêgo - Ryan Silva de Sousa
+f1s1 = []
+f1s2 = []
+f2s1 = []
+f2s2 = []
+f3s1 = []
+f3s2 = []
+
+
 def main()
 print("Menu Principal\n1. Comprar ingressos para Filme 1 - Sessão 1\n2. Comprar ingressos para Filme 1 - Sessão 2\n3. Comprar ingressos para Filme 2 - Sessão 1\n4. Comprar ingressos para Filme 2 - Sessão 2\n5. Comprar ingressos para Filme 3 - Sessão 1\n6. Comprar ingressos para Filme 3 - Sessão 2\n7. Avaliar um filme\n8. Encerrar o dia e exibir o relatório")
