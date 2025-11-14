aac528da9a1310733fdcce8b89fae588f1226b1e
Guigspinho <84827957+Guigspinho@users.noreply.github.com>
2025-11-14 16:35:42 +0000

    versaoSessoesAtualizadas

--- projeto_cinemack.py
+++ projeto_cinemack.py
@@ -13,8 +13,12 @@ contf2s2 = 50
 contf3s1 = 50
 contf3s2 = 50
 
-ingressos = []
-
+ingf1s1 = []
+ingf1s2 = []
+ingf2s1 = []
+ingf2s2 = []
+ingf3s1 = []
+ingf3s2 = []
 
 def entrada():
     print("Menu Principal\n1. Comprar ingressos para Filme 1 - Sessão 1\n2. Comprar ingressos para Filme 1 - Sessão 2\n3. Comprar ingressos para Filme 2 - Sessão 1\n4. Comprar ingressos para Filme 2 - Sessão 2\n5. Comprar ingressos para Filme 3 - Sessão 1\n6. Comprar ingressos para Filme 3 - Sessão 2\n7. Avaliar um filme\n8. Encerrar o dia e exibir o relatório\n")
@@ -32,17 +36,17 @@ def compraring(op):
         while True:
             tipoing = int(input("Escolha o ingresso: "))
             if tipoing == 1:
-                ingressos.append(20)
+                ingf1s1.append(20)
                 f1s1.append("int")
                 contf1s1 -= 1
                 tempf1s1 += 1
             elif tipoing == 2:
-                ingressos.append(10)
+                ingf1s1.append(10)
                 f1s1.append("meia")
                 contf1s1 -= 1
                 tempf1s1 += 1
             elif tipoing == 3:
-                ingressos.append(30)
+                ingf1s1.append(30)
                 f1s1.append("vip")
                 contf1s1 -= 1
                 tempf1s1 += 1
@@ -52,7 +56,7 @@ def compraring(op):
             elif tipoing == 5:
                 contf1s1 += tempf1s1
                 for i in range(tempf1s1 - 1, -1, -1):
-                    del ingressos[-1]
+                    del ingf1s1[-1]
                     del f1s1[-1]    
                 return
 
@@ -65,17 +69,17 @@ def compraring(op):
         while True:
             tipoing = int(input("Escolha o ingresso: "))
             if tipoing == 1:
-                ingressos.append(20)
+                ingf1s2.append(20)
                 f1s2.append("int")
                 contf1s2 -= 1
                 tempf1s2 += 1
             elif tipoing == 2:
-                ingressos.append(10)
+                ingf1s2.append(10)
                 f1s2.append("meia")
                 contf1s2 -= 1
                 tempf1s2 += 1
             elif tipoing == 3:
-                ingressos.append(30)
+                ingf1s2.append(30)
                 f1s2.append("vip")
                 contf1s2 -= 1
                 tempf1s2 += 1
@@ -85,7 +89,7 @@ def compraring(op):
             elif tipoing == 5:
                 contf1s2 += tempf1s2
                 for i in range(tempf1s2 - 1, -1, -1):
-                    del ingressos[-1]
+                    del ingf1s2[-1]
                     del f1s2[-1]    
                 return
             else:
@@ -97,17 +101,17 @@ def compraring(op):
         while True:
             tipoing = int(input("Escolha o ingresso: "))
             if tipoing == 1:
-                ingressos.append(20)
+                ingf2s1.append(20)
                 f2s1.append("int")
                 contf2s1 -= 1
                 tempf2s1 += 1
             elif tipoing == 2:
-                ingressos.append(10)
+                ingf2s1.append(10)
                 f2s1.append("meia")
                 contf2s1 -= 1
                 tempf2s1 += 1
             elif tipoing == 3:
-                ingressos.append(30)
+                ingf2s1.append(30)
                 f2s1.append("vip")
                 contf2s1 -= 1
                 tempf2s1 += 1
@@ -117,29 +121,29 @@ def compraring(op):
             elif tipoing == 5:
                 contf2s1 += tempf2s1
                 for i in range(tempf2s1 - 1, -1, -1):
-                    del ingressos[-1]
+                    del ingf2s1[-1]
                     del f2s1[-1]    
                 return
             else:
                 print("Opção inválida, por favor escolha novamente.")
-    elif op == 3: #Filme 2 - Sessão 2
+    elif op == 4: #Filme 2 - Sessão 2
         global contf2s2
         tempf2s2 = 0
         print(f"Assentos disponíveis para Filme 1 - Sessão 2: {contf2s2}\nEscolha o tipo de ingresso (1: Inteira, 2: Meia, 3: VIP, 4: Confirmar compra, 5: Voltar): ")
         while True:
             tipoing = int(input("Escolha o ingresso: "))
             if tipoing == 1:
-                ingressos.append(20)
+                ingf2s2.append(20)
                 f2s2.append("int")
                 contf2s2 -= 1
                 tempf2s2 += 1
             elif tipoing == 2:
-                ingressos.append(10)
+                ingf2s2.append(10)
                 f2s2.append("meia")
                 contf2s2 -= 1
                 tempf2s2 += 1
             elif tipoing == 3:
-                ingressos.append(30)
+                ingf2s2.append(30)
                 f2s2.append("vip")
                 contf2s2 -= 1
                 tempf2s2 += 1
@@ -149,29 +153,29 @@ def compraring(op):
             elif tipoing == 5:
                 contf2s2 += tempf2s2
                 for i in range(tempf2s2 - 1, -1, -1):
-                    del ingressos[-1]
+                    del ingf2s2[-1]
                     del f2s2[-1]    
                 return
             else:
                 print("Opção inválida, por favor escolha novamente.")
-    elif op == 3: #Filme 3 - Sessão 1
+    elif op == 5: #Filme 3 - Sessão 1
         global contf3s1
         tempf3s1 = 0
         print(f"Assentos disponíveis para Filme 1 - Sessão 2: {contf3s1}\nEscolha o tipo de ingresso (1: Inteira, 2: Meia, 3: VIP, 4: Confirmar compra, 5: Voltar): ")
         while True:
             tipoing = int(input("Escolha o ingresso: "))
             if tipoing == 1:
-                ingressos.append(20)
+                ingf3s1.append(20)
                 f3s1.append("int")
                 contf3s1 -= 1
                 tempf3s1 += 1
             elif tipoing == 2:
-                ingressos.append(10)
+                ingf3s1.append(10)
                 f3s1.append("meia")
                 contf3s1 -= 1
                 tempf3s1 += 1
             elif tipoing == 3:
-                ingressos.append(30)
+                ingf3s1.append(30)
                 f3s1.append("vip")
                 contf3s1 -= 1
                 tempf3s1 += 1
@@ -181,29 +185,29 @@ def compraring(op):
             elif tipoing == 5:
                 contf3s1 += tempf3s1
                 for i in range(tempf3s1 - 1, -1, -1):
-                    del ingressos[-1]
+                    del ingf3s1[-1]
                     del f3s1[-1]    
                 return
             else:
                 print("Opção inválida, por favor escolha novamente.")
-    elif op == 3: #Filme 3 - Sessão 2
+    elif op == 6: #Filme 3 - Sessão 2
         global contf3s2
         tempf3s2 = 0
         print(f"Assentos disponíveis para Filme 1 - Sessão 2: {contf3s2}\nEscolha o tipo de ingresso (1: Inteira, 2: Meia, 3: VIP, 4: Confirmar compra, 5: Voltar): ")
         while True:
             tipoing = int(input("Escolha o ingresso: "))
             if tipoing == 1:
-                ingressos.append(20)
+                ingf3s2.append(20)
                 f3s2.append("int")
                 contf3s2 -= 1
                 tempf3s2 += 1
             elif tipoing == 2:
-                ingressos.append(10)
+                ingf3s2.append(10)
                 f3s2.append("meia")
                 contf3s2 -= 1
                 tempf3s2 += 1
             elif tipoing == 3:
-                ingressos.append(30)
+                ingf3s2.append(30)
                 f3s2.append("vip")
                 contf3s2 -= 1
                 tempf3s2 += 1
@@ -213,7 +217,7 @@ def compraring(op):
             elif tipoing == 5:
                 contf3s2 += tempf3s2
                 for i in range(tempf3s2 - 1, -1, -1):
-                    del ingressos[-1]
+                    del ingf3s2[-1]
                     del f3s2[-1]    
                 return
             else:
@@ -225,6 +229,6 @@ def main():
             print("relatório...")
             break
         compraring(op)
-        print(ingressos, f1s1)
+        print(ingf1s1, f1s1)
 
 main()
