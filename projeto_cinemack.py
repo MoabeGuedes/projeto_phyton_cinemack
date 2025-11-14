643343685fa9e9c0906138564c975a4aad39b7ec
Guigspinho <84827957+Guigspinho@users.noreply.github.com>
2025-11-14 13:58:04 +0000

    versao1dia14

--- projeto_cinemack.py
+++ projeto_cinemack.py
@@ -1,14 +1,22 @@
-#Guilherme Gomes Pinho - Moabe da Silva Guedes Rêgo - Ryan Silva de Sousa
+#Projeto 2 (Guilherme Gomes Pinho - Moabe da Silva Guedes Rêgo - Ryan Silva de Sousa)
 f1s1 = []
-contf1s1 = 50
-
 f1s2 = []
 f2s1 = []
 f2s2 = []
 f3s1 = []
 f3s2 = []
 
+ingressos = []
+
+contf1s1 = 50
+contf1s2 = 50
+contf2s1 = 50
+contf2s2 = 50
+contf3s1 = 50
+contf3s2 = 50
+
 def entrada():
+    print("Menu Principal\n1. Comprar ingressos para Filme 1 - Sessão 1\n2. Comprar ingressos para Filme 1 - Sessão 2\n3. Comprar ingressos para Filme 2 - Sessão 1\n4. Comprar ingressos para Filme 2 - Sessão 2\n5. Comprar ingressos para Filme 3 - Sessão 1\n6. Comprar ingressos para Filme 3 - Sessão 2\n7. Avaliar um filme\n8. Encerrar o dia e exibir o relatório\n")
     op = int(input("Escolha a opção desejada: "))
     while op <=0 or op >= 9:
         op = int(input("Opção inválida, escolha a opção desejada (1-8): "))
@@ -18,15 +26,27 @@ def entrada():
 def compraring(op):
     if op == 1:
         global contf1s1
-        ingressos = []
-        print(f"Assentos disponíveis para Filme 1 - Sessão 1: {cont}\nEscolha o tipo de ingresso (1: Inteira, 2: Meia, 3: VIP, 4: Confirmar compra, 5: Voltar): ")
-        tipoing = int(input(""))
-        for i in range(cont):
+        
+        print(f"Assentos disponíveis para Filme 1 - Sessão 1: {contf1s1}\nEscolha o tipo de ingresso (1: Inteira, 2: Meia, 3: VIP, 4: Confirmar compra, 5: Voltar): ")
+        while True:
+            tipoing = int(input())
             if tipoing == 1:
-                ingressos[i].append(20)
-                f1s1.append(int)
-
+                ingressos.append(20)
+                f1s1.append("int")
+            if tipoing == 2:
+                ingressos.append(10)
+                f1s1.append("meia")
+            if tipoing == 3:
+                ingressos.append(30)
+                f1s1.append("vip")
+            if tipoing == 4:
+                break
+            if tipoing == 5:
+                compraring(entrada())
 
 def main():
-    print("Menu Principal\n1. Comprar ingressos para Filme 1 - Sessão 1\n2. Comprar ingressos para Filme 1 - Sessão 2\n3. Comprar ingressos para Filme 2 - Sessão 1\n4. Comprar ingressos para Filme 2 - Sessão 2\n5. Comprar ingressos para Filme 3 - Sessão 1\n6. Comprar ingressos para Filme 3 - Sessão 2\n7. Avaliar um filme\n8. Encerrar o dia e exibir o relatório")
     op = entrada()
+    compraring(op)
+    print(ingressos, f1s1)
+    
+main()
